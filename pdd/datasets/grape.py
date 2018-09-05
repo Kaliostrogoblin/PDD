6ad576b2a4eda0eda8e2628fd9a5edba4b642f1f
Гончаров Павел Владимирович <pgoncharov@intervale.ru>
2018-09-05 17:34:02 +0300

    Dataset loading function was created

---
+++ pdd/datasets/grape.py
@@ -0,0 +1,8 @@
+"""MNIST handwritten digits dataset.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from ..utils.data_utils import get_file
+import numpy as np
