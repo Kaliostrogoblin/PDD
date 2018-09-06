e923fa2cf97c4747b5b7bd432a4284e3451b71cf
Гончаров Павел Владимирович <pgoncharov@intervale.ru>
2018-09-06 15:20:23 +0300

    Splitting data on train and test while downloading

--- pdd/datasets/grape.py
+++ pdd/datasets/grape.py
@@ -5,19 +5,40 @@ from __future__ import division
 from __future__ import print_function
 
 from ..utils.data_utils import get_file
+from ..utils.data_utils import datadir_train_test_split
 import numpy as np
+import sys
 
 
-def load_data(path='grape.tar'):
+def load_data(path='grape.tar', 
+              split_on_train_test=False, 
+              test_size=None, 
+              random_state=0):
     """Loads the PDD grape dataset.
     # Arguments
         path: path where to cache the dataset locally
             (relative to ~/.pdd/datasets).
+        split_on_train_test: flag, controls whether or not
+            data should be splitted on train and test
+        test_size: the size of test data fraction
     # Returns
-        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+        Path to the folder with data or tuple with train and test paths
     """
     path = get_file(path,
                     origin="http://pdd.jinr.ru/images/base/grape/grape.tar",
-                    file_hash='cbc74aef6e5f20ef22b41d1f0f77e40567e6fd1f4e2c007341df9fb4672c3fd8')
+                    file_hash='cbc74aef6e5f20ef22b41d1f0f77e40567e6fd1f4e2c007341df9fb4672c3fd8',
+                    extract=True)
 
+    try:
+        if split_on_train_test:
+            print("Splitting on train and test...")
+            test_size = 0.15 if test_size is None else test_size
+            train_path, test_path = datadir_train_test_split(
+                path, test_size, random_state)
+            return (train_path, test_path)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
     
+    return path
+
+
--- pdd/utils/data_utils.py
+++ pdd/utils/data_utils.py
@@ -2,10 +2,12 @@ import hashlib
 import os
 import requests
 import six
+import shutil
 import tarfile
 import zipfile
 
 import numpy as np
+from glob import glob
 from scipy.ndimage import imread
 from tqdm import tqdm
 
@@ -65,6 +67,14 @@ def create_dataset_from_dir(dirname, shuffle=False, **kwargs):
     return dataset
 
 
+def _remove_path_if_exists(path):
+    if os.path.exists(path):
+        if os.path.isfile(path):
+            os.remove(path)
+        else:
+            shutil.rmtree(path)
+
+
 def _extract_archive(file_path, path='.', archive_format='auto'):
     """Extracts an archive if it matches tar, tar.gz, tar.bz, or zip formats.
     # Arguments
@@ -100,11 +110,7 @@ def _extract_archive(file_path, path='.', archive_format='auto'):
                     archive.extractall(path)
                 except (tarfile.TarError, RuntimeError,
                         KeyboardInterrupt):
-                    if os.path.exists(path):
-                        if os.path.isfile(path):
-                            os.remove(path)
-                        else:
-                            shutil.rmtree(path)
+                    _remove_path_if_exists(path)
                     raise
             return True
     return False
@@ -206,7 +212,10 @@ def get_file(fname,
             raise
 
     if extract:
-        _extract_archive(fpath, os.path.splitext(fpath)[0])
+        datadir_path = os.path.splitext(fpath)[0]
+        _remove_path_if_exists(datadir_path)
+        _extract_archive(fpath, datadir_path)
+        return datadir_path
 
     return fpath
 
@@ -261,3 +270,67 @@ def validate_file(fpath, file_hash, algorithm='auto', chunk_size=65535):
         return True
     else:
         return False
+
+
+def _makedir_and_copy2(path, dirname, fnames):
+    path_for_saving_files = os.path.join(path, dirname)
+    os.makedirs(path_for_saving_files)
+
+    for fname in fnames:
+        shutil.copy2(fname, path_for_saving_files)
+
+
+def datadir_train_test_split(origin_path, test_size, random_state=0):
+    """Splits the data in directory on train and test.
+    # Arguments
+        origin_path: path to the original directory
+        test_size: the size of test data fraction
+    # Returns
+        Tuple of paths: `(train_path, test_path)`.
+    """
+    train_path = os.path.join(origin_path, 'train')
+    test_path = os.path.join(origin_path, 'test')   
+    _remove_path_if_exists(train_path)
+    _remove_path_if_exists(test_path)
+
+    try:
+        subfolders = glob(os.path.join(origin_path, "*", ""))
+        # if train/test split is already done
+        if set(subfolders) == set(['train', 'test']):
+            return (train_path, test_path)
+        # if train/test split is required
+        # recreate train/test folders
+        os.makedirs(train_path)
+        os.makedirs(test_path)
+
+        for folder in tqdm(subfolders, total=len(subfolders), ncols=57):
+            # collect all images
+            img_fnames = []
+            for ext in ["*.jpg", "*.png", "*jpeg"]:
+                img_fnames.extend(
+                    glob(os.path.join(origin_path, folder, ext)))
+            # set random state parameter
+            rs = np.random.RandomState(random_state)
+            # shuffle array
+            rs.shuffle(img_fnames)
+            # split on train and test
+            n_test_files = int(len(img_fnames)*test_size)
+            test_img_fnames = img_fnames[:n_test_files]
+            train_img_fnames = img_fnames[n_test_files:]
+            # copy train files into `train_path/folder`
+            folder_name = os.path.basename(os.path.dirname(folder))
+            _makedir_and_copy2(train_path, folder_name, train_img_fnames)
+            # copy test files into `test_path/folder`
+            _makedir_and_copy2(test_path, folder_name, test_img_fnames)
+
+        for folder in subfolders:
+            shutil.rmtree(folder)
+
+    except:
+        _remove_path_if_exists(train_path)
+        _remove_path_if_exists(test_path)
+        raise 
+
+    return (train_path, test_path)
+
+
