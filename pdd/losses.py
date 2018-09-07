fea91feceae0f1e97d39494524076583c459ed35
Гончаров Павел Владимирович <pgoncharov@intervale.ru>
2018-09-07 13:15:34 +0300

    contrastive loss was fixed

--- pdd/losses.py
+++ pdd/losses.py
@@ -8,4 +8,4 @@ def contrastive_loss(y_true, y_pred):
     m = 1.0   # margin
     between_class = (1-y_true) * K.square(y_pred)      # (1-Y)*(d^2)
     within_class = y_true * K.square(K.maximum(m-y_pred, 0))  # (Y) * max((margin - d)^2, 0)
-    return K.mean(within_class + between_class)
+    return within_class + between_class
